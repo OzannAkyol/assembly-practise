31a934cc031320d18b5eb05904f8f4bfd9f69733
OzannAkyol <149076594+OzannAkyol@users.noreply.github.com>
2026-04-29 18:28:40 +0000

    feat(context-switching): List based context switching added.

--- main.c
+++ main.c
@@ -1,5 +1,69 @@
-#include "xrt_thread.h"
+/* USER CODE BEGIN Header */
+/**
+  **************************
+  * @file           : main.c
+  * @brief          : Main program body
+  **************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  **************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+//#include "i2c.h"
+//#include "i2s.h"
+//#include "spi.h"
+//#include "usb_host.h"
+#include "gpio.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include "cmsis_gcc.h"
+#include <stdbool.h>
+#include "xrt_thread.h"
+#include "xrt_list.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void MX_USB_HOST_Process(void);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 
 #define NUM_OF_THREAD_IN_SYSTEM (4)
 
@@ -27,7 +91,10 @@ void t1_dummy_function(void){
 	(void)b;
 
 	while(1){
-		HAL_GPIO_WritePin(LED_TEST_GPIO_Port, LED_TEST_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
 	}
 }
 
@@ -35,25 +102,61 @@ void t2_dummy_function(void){
 	int a = 5;
 	int b = 20;
 
+	(void)a;
+	(void)b;
+
 	while(1){
-		HAL_GPIO_WritePin(LED_TEST_GPIO_Port, LED_TEST_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
+	}
+}
+
+void t3_dummy_function(void){
+	int a = 5;
+	int b = 20;
+
+	(void)a;
+	(void)b;
+
+	while(1){
+		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+	}
+}
+
+void t4_dummy_function(void){
+	int a = 5;
+	int b = 20;
+
+	(void)a;
+	(void)b;
+
+	while(1){
+		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
+
 	}
 }
 
 TCB_t tcb1 = {
         .fptr = t1_dummy_function,
         .thread_sp = thread1_stack + THREAD1_STACK_SIZE - 1,
-        .ThreadStackSize = THREAD1_STACK_SIZE * 4,
-        .priority = LOW_PRIORITY,
+        .ThreadStackSize = THREAD1_STACK_SIZE,
+        .priority = HIGH_PRIORITY,
         .state = THREAD_READY_STATE,
         .thread_id = "T1",
         .thread_base_ptr = thread1_stack,
 };
 
-
 TCB_t tcb2 = {
         .fptr = t2_dummy_function,
-        .ThreadStackSize = THREAD2_STACK_SIZE * 4,
+        .ThreadStackSize = THREAD2_STACK_SIZE,
         .priority = HIGH_PRIORITY,
         .state = THREAD_READY_STATE,
         .thread_id = "T2",
@@ -71,38 +174,56 @@ TCB_t tcb3 = {
         .thread_sp = thread3_stack + THREAD3_STACK_SIZE - 1,
 };
 
-cdll_node thread1_node = {
-		.data = &tcb1,
-		.next = NULL,
-		.prev = NULL,
+TCB_t tcb4 = {
+        .fptr = t4_dummy_function,
+        .ThreadStackSize = THREAD4_STACK_SIZE,
+        .priority = HIGH_PRIORITY,
+        .state = THREAD_READY_STATE,
+        .thread_id = "T4",
+        .thread_base_ptr = thread4_stack,
+        .thread_sp = thread4_stack + THREAD4_STACK_SIZE - 1,
 };
 
-cdll_node thread2_node = {
-		.data = &tcb2,
-		.next = NULL,
-		.prev = NULL,
+
+cdll_list list;
+cdll_node thread1_node ={
+	.data = &tcb1,
+	.prev = NULL,
+	.next = NULL,
 };
 
-cdll_node thread3_node = {
-		.data = &tcb3,
-		.next = NULL,
-		.prev = NULL,
+cdll_node thread2_node ={
+	.data = &tcb2,
+	.prev = NULL,
+	.next = NULL,
 };
 
-TCB_List_t xrtKernelList = {
-							.head = NULL,
-							.size = 0,
+cdll_node thread3_node ={
+	.data = &tcb3,
+	.prev = NULL,
+	.next = NULL,
 };
 
-__STATIC_FORCEINLINE void xrt_thread_start();
-__STATIC_FORCEINLINE void xrt_thread_start(){
-	uint32_t value = (uint32_t)tcb1.thread_sp;
-	__set_PSP(value);
-	__set_CONTROL(__get_CONTROL() | 0x2);
+cdll_node thread4_node ={
+	.data = &tcb4,
+	.prev = NULL,
+	.next = NULL,
+};
 
-	tcb1.fptr();
+TCB_List_t xrtKernelReadyList={
+		.head = NULL,
+		.size = 0,
+};
 
-}
+TCB_List_t xrtKernelRunningList ={
+		.head = NULL,
+		.size = 0,
+};
+
+TCB_List_t xrtKernelStoppedList ={
+		.head = NULL,
+		.size = 0,
+};
 
 
 /* USER CODE END 0 */
@@ -115,20 +236,9 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+//  __disable_irq();
 	  FPU->FPCCR &= ~(FPU_FPCCR_LSPEN_Msk | FPU_FPCCR_ASPEN_Msk);
-	__enable_irq();
   /* USER CODE END 1 */
-
-  /* MPU Configuration--------------------------------------------------------*/
-//  MPU_Config();
-
-  /* Enable the CPU Cache */
-
-  /* Enable I-Cache---------------------------------------------------------*/
-//  SCB_EnableICache();
-
-  /* Enable D-Cache---------------------------------------------------------*/
-//  SCB_EnableDCache();
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -142,37 +252,129 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* Configure the peripherals common clocks */
-  PeriphCommonClock_Config();
-
   /* USER CODE BEGIN SysInit */
-	MX_GPIO_Init();
 
+  /* USER CODE END SysInit */
 
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  //change the MSP to PSP
   for(int i = 0; i < THREAD2_STACK_SIZE; ++i){
 	  thread1_stack[i] = 11;
 	  thread2_stack[i] = 11;
+	  thread3_stack[i] = 11;
+	  thread4_stack[i] = 11;
   }
 
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   /* USER CODE BEGIN 2 */
-  	xrt_thread_list_init(&xrtKernelList);
+  xrt_thread_list_init(&xrtKernelRunningList);
+  xrt_thread_list_init(&xrtKernelStoppedList);
 
-	xrt_thread_init(&xrtKernelList, &tcb1, &thread1_node);
-	xrt_thread_init(&xrtKernelList, &tcb2, &thread2_node);
+  xrt_thread_init(&xrtKernelRunningList, &tcb1, &thread1_node);
 
-	xrt_thread_start();
+  xrt_thread_list_init(&xrtKernelReadyList);
+  xrt_thread_init(&xrtKernelReadyList, &tcb2, &thread2_node);
+  xrt_thread_init(&xrtKernelReadyList, &tcb3, &thread3_node);
+  xrt_thread_init(&xrtKernelReadyList, &tcb4, &thread4_node);
+//  cdll_sort_list(&xrtKernelReadyList);
+  xrt_thread_start();
+
+//  SCB -> SHCSR |= SCB_SHCSR_SVCALLPENDED_Msk;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+  while(1)
   {
+
     /* USER CODE END WHILE */
+    // MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
